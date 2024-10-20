631f171d706c24951267e888454f294f1c919217
Amit Hasan Sikder <amithasan16hstu@gmail.com>
2024-10-20 17:14:16 +0600

    Add files via upload

---
+++ Homepractice3.sql
@@ -0,0 +1,9 @@
+create table booklist(
+number_id int,
+book_name varchar(30),
+book_title varchar(30),
+page_number int
+);
+insert into booklist(number_id,book_name,book_title,page_number)
+values(101,"C++","C++",120),(102,"Concept of Database","DBMS",140),(103,"Modern Electronic Device","ECE",150),(104,"Digital Logic","DLD",150),(105,"Python Concept","Python",200),
+(106,"Concept of Data Structure","DSA",220),(107,"Algorithm Design","DS",250);
